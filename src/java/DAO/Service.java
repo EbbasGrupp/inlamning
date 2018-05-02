c2ba48aa527f5922412d3dd4aec060f3e4de1064
EbbasGrupp <38653505+EbbasGrupp@users.noreply.github.com>
2018-05-02 15:13:13 +0200

    Service fixat

--- src/java/DAO/Service.java
+++ src/java/DAO/Service.java
@@ -1,19 +1,61 @@
 package DAO;
 
 import DAO.Data.*;
+import java.util.ArrayList;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("Service")
 public class Service {
     DataDAO datDAO = new DataDAO();
     
-    
     @GET
-    @Path("Temperatuuure")
+    @Path("TemperatureLastHour")
     @Produces(MediaType.APPLICATION_JSON)
     public Temperature getTempLastHour(){
         Temperature temp = datDAO.getTempLastHour();
         return temp;
+    }
+    
+    @GET
+    @Path("HumidityLastHour")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Humidity getHumLastHour(){
+        Humidity hum = datDAO.getHumLastHour();
+        return hum;
+    }
+    
+    @GET
+    @Path("LightLastHour")
+    @Produces(MediaType.APPLICATION_JSON)
+    public LightIntensity getLightLastHour(){
+        LightIntensity light = datDAO.getLightLastHour();
+        return light;
+    }
+    
+    
+    @GET
+    @Path("TempLastDay")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<Temperature> getLastDayTemp(){
+        ArrayList<Temperature> tempList = datDAO.getLastDayTemp();
+        return tempList;
+    }
+    
+    @GET
+    @Path("HumLastDay")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<Humidity> getLastDayHum(){
+        ArrayList<Humidity> humList = datDAO.getLastDayHum();
+        return humList;
+    }
+    
+    @GET
+    @Path("LightLastDay")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<LightIntensity> getLastDayLight(){
+        ArrayList<LightIntensity> lightList = datDAO.getLastDayLight();
+        return lightList;
     }
 }
